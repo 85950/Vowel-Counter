168e43d9464c09c3a93a3e3367b16a3d60a881df
85950 <70612510+85950@users.noreply.github.com>
2020-09-20 17:04:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ function check(){
     var temp = userInput.value;
     const vowels = /[a,e,i,o,u,A,E,I,O,U]/g;
     var count = temp.match(vowels).length;
-    alert(`There are ${count} vowels!`);
+    if (count === 1){
+        alert(`There is ${count} vowel.`)
+    }
+    else{
+        alert(`There are ${count} vowels!`);
+    }
 }
 //main
 main()
-
